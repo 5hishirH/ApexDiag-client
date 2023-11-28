3d04bb3486c4876a943e909d0fd93ecec65c4ea7
Shishir Halder <s.halder0108@gmail.com>
2023-11-28 13:09:25 +0600

    feat: add styles to all users route

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -5,7 +5,6 @@ import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
   const axiosPublic = useAxiosPublic();
-  // const [userData, setUserData] = useState();
 
   const { data: userData = [], refetch } = useQuery({
     queryKey: ["userData"],
@@ -14,15 +13,6 @@ const AllUsers = () => {
       return res.data;
     },
   });
-
-  // useEffect(() => {
-  //   axiosPublic
-  //     .get("/users")
-  //     .then((res) => {
-  //       setUserData(res.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
 
   const makeAdmin = (id, role) => {
     if (role === "admin") {
@@ -52,7 +42,7 @@ const AllUsers = () => {
 
   const handleStatus = (id, status) => {
     if (status === "active") {
-      status = "inactive";
+      status = "blocked";
     } else {
       status = "active";
     }
@@ -76,30 +66,105 @@ const AllUsers = () => {
 
   return (
     <div className="p-8 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-      {userData?.map((e, index) => (
-        <div
-          key={index}
-          className="w-full flex justify-between items-center mt-4 border-2 border-primary rounded-md px-6 py-4"
-        >
-          <div>{e?.userName}</div>
-          <div>{e?.mail}</div>
-          <button className="btn btn-accent btn-sm">See Info</button>
-          <button
-            onClick={() => makeAdmin(e?._id, e?.role)}
-            className="btn btn-info btn-sm"
-          >
-            make {e?.role === "admin" ? "normal" : "admin"}
-          </button>
-          <button
-            onClick={() => handleStatus(e?._id, e?.status)}
-            className={`btn ${
-              e?.status === "active" ? "btn-success" : "btn-error"
-            } btn-sm`}
-          >
-            {e?.status === "active" ? "Active" : "Inactive"}
-          </button>
-        </div>
-      ))}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Info</th>
+              <th>Change role</th>
+              <th>Change status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData?.map((e, index) => (
+              <tr key={index}>
+                <th>{index+1}</th>
+                <th>{e?.userName}</th>
+                <td>{e?.mail}</td>
+                <td>
+                  <div>
+                    <button
+                      className="btn btn-accent btn-sm"
+                      onClick={() =>
+                        document.getElementById(`my_modal_${index}`).showModal()
+                      }
+                    >
+                      See Info
+                    </button>
+                    <dialog id={`my_modal_${index}`} className="modal">
+                      <div className="modal-box">
+                        <form method="dialog">
+                          {/* if there is a button in form, it will close the modal */}
+                          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                            ✕
+                          </button>
+                        </form>
+                        <div className="flex items-center gap-6">
+                          <div className="avatar">
+                            <div className="w-16 rounded-full">
+                              <img src={e?.imgURL} />
+                            </div>
+                          </div>
+                          <div>
+                            <h3 className="text-xl">{e?.userName}</h3>
+                            <p className="italic">{e?.mail}</p>
+                          </div>
+                        </div>
+                        <div className="mt-6 pl-2">
+                          <p>
+                            <span className="font-medium">Blood Group : </span>
+                            <span>{e?.bloodG}</span>
+                          </p>
+                          <p>
+                            <span className="font-medium">Upazila : </span>
+                            <span>{e?.upa}</span>
+                          </p>
+                          <p>
+                            <span className="font-medium">Role : </span>
+                            <span>{e?.role}</span>
+                          </p>
+                          <p>
+                            <span className="font-medium">Status : </span>
+                            <span>{e?.status}</span>
+                          </p>
+                        </div>
+                      </div>
+                    </dialog>
+                  </div>
+                </td>
+                <td>
+                  <div className="w-32">
+                    <button
+                      onClick={() => makeAdmin(e?._id, e?.role)}
+                      className="btn btn-block btn-info btn-sm"
+                    >
+                      Make {e?.role === "admin" ? "normal" : "admin"}
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <div className="w-20">
+                    <button
+                      onClick={() => handleStatus(e?._id, e?.status)}
+                      className={`btn btn-block ${
+                        e?.status === "active"
+                          ? "btn-success"
+                          : "btn-error text-white"
+                      } btn-sm`}
+                    >
+                      {e?.status === "active" ? "Active" : "blocked"}
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
